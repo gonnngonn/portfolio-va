47e2c81b998d5183ce2fc5bcbe4e994675042e1f
gonnngonn <gonzalo.gamboa233@gmail.com>
2024-11-12 22:16:28 -0300

    Portfolio Fixed

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 });
