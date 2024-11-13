001aefae26636db2a1112b16440901213fa4d031
gonnngonn <gonzalo.gamboa233@gmail.com>
2024-11-12 23:00:20 -0300

    Portfolio Reactualizado

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  publicDir: "public",
 });
